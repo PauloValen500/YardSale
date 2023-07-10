ae3deec82e06a6a2e58fcfd5623ae4e70cc5be9e
Paulo Valenzuela <paulovalensuela@hotmail.com>
2023-07-09 20:48:19 -0600

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuArrow = document.querySelector('.menu-arrow');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuArrow.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');  
 }
